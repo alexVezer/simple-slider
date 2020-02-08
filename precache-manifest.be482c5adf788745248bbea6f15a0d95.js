fec59bd5d123b8b86617e773c55d5020ce769c1d
alexVezer <alexartimon@gmail.com>
2020-02-08 22:23:59 +0200

    Updates

---
+++ precache-manifest.be482c5adf788745248bbea6f15a0d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1211adf4e3ca3b4eb5cb04a7ef39eb65",
+    "url": "/simple-slider/index.html"
+  },
+  {
+    "revision": "86602594ce285b5b97ee",
+    "url": "/simple-slider/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5ef840d33460a736f470",
+    "url": "/simple-slider/static/css/main.11d2ee32.chunk.css"
+  },
+  {
+    "revision": "86602594ce285b5b97ee",
+    "url": "/simple-slider/static/js/2.7e642f69.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-slider/static/js/2.7e642f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef840d33460a736f470",
+    "url": "/simple-slider/static/js/main.cef260ae.chunk.js"
+  },
+  {
+    "revision": "430fd739ce20c4e21af5",
+    "url": "/simple-slider/static/js/runtime-main.a37c0cec.js"
+  }
+]);
